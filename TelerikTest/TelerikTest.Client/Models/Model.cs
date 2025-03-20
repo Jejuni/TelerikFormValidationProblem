26db495f98d0878bf958cdf3b77ff9f727cef98e
Daniel Boos <JejuniProgramming@gmail.com>
2025-03-20 15:32:42 +0100

    Add project files.

---
+++ TelerikTest/TelerikTest.Client/Models/Model.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace TelerikTest.Client.Models;
+
+public class ModelValidator : AbstractValidator<Model>
+{
+    private readonly List<int> _values = [1, 2, 3];
+
+    public ModelValidator()
+    {
+        RuleFor(request => request.Name).NotEmpty();
+        RuleFor(request => request.Brand)
+            .Must(_values.Contains)
+            .WithMessage($"Brand must be one of: {string.Join(", ", _values)}");
+    }
+}
+
+public sealed record Model
+{
+    public static Model Empty => new() { Id = Guid.Empty, Name = "", Brand = 0 };
+
+    public required string Name { get; init; }
+
+    public required int Brand { get; set; }
+
+    [Display(AutoGenerateField = false)] public required Guid Id { get; init; }
+}
---
+++ TelerikTest/TelerikTest.Client/Program.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddTelerikBlazor();
+await builder.Build().RunAsync();
